82c8e21d63a84130d3b761179580f27670f4dc45
vijay <vijyamca.2406@gmail.com>
2025-07-27 08:08:07 -0700

    terraformtask

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "create_files" {  
+
+provisioner "local-exec" {  
+
+command = "touch file1.txt file2.txt" } 
+
+ } 
+
+resource "null_resource" "create_directories" {  
+
+provisioner "local-exec" {  
+
+command = "mkdir dir1 dir2" } 
+
+ } 
