ab627d546ab0abc21ba93380f37a1989d2df7efb
VM240697 <vijaymca.2406@gmail.com>
2025-08-22 08:42:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "alert" {
   name     = "Alert"
   location = "East US"
 }
@@ -10,21 +10,21 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "example" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.alert.location
+  resource_group_name = azurerm_resource_group.alert.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = azurerm_resource_group.alert.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "example" {
   name                = "example-vmss"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.alert.name
+  location            = azurerm_resource_group.alert.location
   sku                 = "Standard_DS1_v2"
   instances           = 2
   upgrade_policy_mode = "Automatic"
@@ -48,6 +48,25 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
     ip_configuration {
       name      = "example-ip-config"
       subnet_id = azurerm_subnet.example.id
-    }
-  }
+    }
+  }
+}
+
+resource "azurerm_consumption_budget_resource_group" "example" {
+  name              = "example-budget"
+  resource_group_id = azurerm_resource_group.alert.id
+
+  amount     = 100
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2024-01-01T00:00:00Z"
+  }
+
+  notification {
+    enabled        = true
+    threshold      = 0.8
+    operator       = "EqualTo"
+    contact_emails = ["user@example.com"]
+  }
 }
